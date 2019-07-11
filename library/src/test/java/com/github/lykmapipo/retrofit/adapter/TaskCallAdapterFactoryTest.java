f2f674e5779d0e1785cd0baf6ce34ca0e6bcf7e1
lykmapipo <lallyelias87@gmail.com>
2019-07-11 17:59:13 +0300

    test: implement specs for task call adapter factory

--- library/src/test/java/com/github/lykmapipo/retrofit/adapter/TaskCallAdapterFactoryTest.java
+++ library/src/test/java/com/github/lykmapipo/retrofit/adapter/TaskCallAdapterFactoryTest.java
@@ -1,45 +1,111 @@
 package com.github.lykmapipo.retrofit.adapter;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import com.google.android.gms.tasks.Task;
+import com.google.common.reflect.TypeToken;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import okhttp3.mockwebserver.MockWebServer;
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
+import retrofit2.CallAdapter;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
-@RunWith(RobolectricTestRunner.class)
-public class TaskCallAdapterFactoryTest {
-    private MockWebServer mockWebServer;
-    private String baseUrl;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.fail;
+
+public final class TaskCallAdapterFactoryTest {
+    private static final Annotation[] NO_ANNOTATIONS = new Annotation[0];
+
+    @Rule
+    public final MockWebServer server = new MockWebServer();
+
+    private final CallAdapter.Factory factory = TaskCallAdapterFactory.create();
+    private Retrofit retrofit;
 
     @Before
-    public void setup() throws Exception {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
+    public void setUp() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(server.url("/"))
+                .addConverterFactory(new StringConverterFactory())
+                .addCallAdapterFactory(factory)
+                .build();
     }
 
-    @Before
-    public void before() throws Exception {
-        baseUrl = mockWebServer.url("/v1/").toString();
+    @Test
+    public void responseType() {
+        Type bodyClass = new TypeToken<Task<String>>() {
+        }.getType();
+        assertThat(factory.get(bodyClass, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
+
+        Type bodyWildcard = new TypeToken<Task<? extends String>>() {
+        }.getType();
+        assertThat(factory.get(bodyWildcard, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
+
+        Type bodyGeneric = new TypeToken<Task<List<String>>>() {
+        }.getType();
+        assertThat(factory.get(bodyGeneric, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(new TypeToken<List<String>>() {
+                }.getType());
+
+        Type responseClass = new TypeToken<Task<Response<String>>>() {
+        }.getType();
+        assertThat(factory.get(responseClass, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
+
+        Type responseWildcard = new TypeToken<Task<Response<? extends String>>>() {
+        }.getType();
+        assertThat(factory.get(responseWildcard, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
+
+        Type resultClass = new TypeToken<Task<Response<String>>>() {
+        }.getType();
+        assertThat(factory.get(resultClass, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
+
+        Type resultWildcard = new TypeToken<Task<Response<? extends String>>>() {
+        }.getType();
+        assertThat(factory.get(resultWildcard, NO_ANNOTATIONS, retrofit).responseType())
+                .isEqualTo(String.class);
     }
 
-    @After
-    public void tearDown() throws Exception {
-        mockWebServer.shutdown();
-        baseUrl = null;
-        mockWebServer = null;
+    @Test
+    public void nonListenableTaskReturnsNull() {
+        CallAdapter<?, ?> adapter = factory.get(String.class, NO_ANNOTATIONS, retrofit);
+        assertThat(adapter).isNull();
     }
 
-    public interface Api {
-        @GET("users")
-        @Headers({
-                "Accept: application/json",
-        })
-        Call<List<User>> list();
+    @Test
+    public void rawTypeThrows() {
+        Type observableType = new TypeToken<Task>() {
+        }.getType();
+        try {
+            factory.get(observableType, NO_ANNOTATIONS, retrofit);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e).hasMessage(
+                    "Task return type must be parameterized as Task<Foo> or Task<? extends Foo>");
+        }
+    }
+
+    @Test
+    public void rawResponseTypeThrows() {
+        Type observableType = new TypeToken<Task<Response>>() {
+        }.getType();
+        try {
+            factory.get(observableType, NO_ANNOTATIONS, retrofit);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e).hasMessage(
+                    "Response must be parameterized as Response<Foo> or Response<? extends Foo>");
+        }
     }
 }
+
